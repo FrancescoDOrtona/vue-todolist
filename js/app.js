3073893d01b0ee32795e5a67fec9d364246a7b7e
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-25 14:37:55 +0200

    Add: js vue createApp & array of obj.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
-todos: [
-	{
-		text: 'Fare i compiti',
-		done: false
-	},
-	{
-		text: 'Fare la spesa',
-		done: true
-	},
-	{
-		text: 'Fare il bucato',
-		done: false
-	}
-]
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            logoPath: "./img/Logo.png",
+            newTask: '',
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
+        }
+    },
+}).mount('#app')
